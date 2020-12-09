5fe0b97af4811e3ee8f5f85f146a5f47d505d2e6
sumengzhang <sumengz@student.unimelb.edu.au>
2020-12-09 23:29:06 +0800

    finish 62 dp problem

--- Daily/UniquePaths.java
+++ Daily/UniquePaths.java
@@ -8,19 +8,31 @@ import java.util.Queue;
  * 来源：力扣（LeetCode)
  * 链接：https://leetcode-cn.com/problems/unique-paths
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 
+ *  f(x,y) = f(x-1,y)+f(x,y+1) 转换方程
+ *   
+ *  1->m 的 f(0,m)为 1, 1->n 的 f(m-1,n) 为1；
  */
 public class UniquePaths {
     public static int uniquePaths(int m, int n) {
-        int [][]arr = new int[m][n];
-        
-
-
-
-        return 0;
+        int [][]arr = new int[n][m];
+        for(int i = 0;i<m;i++){
+            arr[0][i] = 1;
+        }
+        for(int j = 0;j<n;j++){
+            arr[j][m-1] =1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=m-2;j>=0;j--){
+                arr[i][j] = arr[i-1][j]+arr[i][j+1];
+            }
+        }
+        return arr[n-1][0];
 
     }
     public static void main(String[] args) {
-       
+       int res = uniquePaths(3,7);
+       System.out.println(res);
 
         
     }

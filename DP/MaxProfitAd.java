d62ebf1efd346fa5ce050c74784b13928e948bc6
sumengzhang <1289969211@qq.com>
2020-12-29 15:00:26 +0800

    maxProfit leetcode 188

---
+++ DP/MaxProfitAd.java
@@ -0,0 +1,41 @@
+/**
+ *  leetcode 188
+ * 给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格。
+
+设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。
+
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MaxProfitAd {
+    /**
+     * 
+     * 当前手上有股票：
+     * 1. 卖
+     * 2. 不卖
+     * 当前受伤无股票
+     * 1. 买
+     * 2. 不买
+     * @param k
+     * @param prices
+     * @return
+     */
+    public static int maxProfit(int k, int[] prices) {
+        
+         
+         
+         
+
+        
+         return 0;
+    }
+    public static void main(String[] args) {
+        int []prices = {3,2,6,5,0,3};
+        int k = 2;
+        System.out.println(maxProfit(k, prices));
+        
+    }
+}

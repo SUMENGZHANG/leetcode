d61e583ea9db3e8a3b98c2ebaf7dd1d4915cb53e
sumengzhang <sumengz@student.unimelb.edu.au>
2021-02-23 23:45:12 +0800

    update

--- ArrayPart/SetZeroes.java
+++ ArrayPart/SetZeroes.java
@@ -1,10 +1,117 @@
+/**
+ * leetcode 73
+ * 
+ * 
+ */
 class SetZeros{
+    /**
+     * 使用额外的空间
+     *  Time:O(m*n)
+     *  Space:O(m+n)
+     * @param matrix
+     */
     public void setZeroes(int[][] matrix){
-         
+         // 通过容器存储需要被置换的行和列
+         Set<Integer> columnSet = new HashSet<>();
+         Set<Integer> rowSet = new HashSet<>();
+       
          int m = matrix.length;
          int n = matrix[0].length;
          for(int i = 0;i<m;i++){
+             for(int j = 0;j<n;j++){
+                 if(matrix[m][n]==0){
+                     columnSet.add(n);
+                     rowSet.add(m);
+                 }
+             } 
+         }
+         // 通过set的数据去置换
+         for (int val : columnSet) {
+             for(int i = 0;i<m;i++){
+                 matrix[i][val]=0;
+             }
              
          }
+         for(int val : rowSet){
+             for(int i = 0;i<n;i++){
+                 matrix[val][i]=0;
+             }
+         }
+    }
+
+   /**
+    * 不使用额外的空间 遍历到之后不存，而是在原地做标记
+    * @param matrix
+    */
+    public static void setZerosSimple(int[][] matrix){
+        boolean rowSet = false;
+        boolean colSet = false;
+        // 判断第一行和第一列要不要变成 0 
+        if(matrix[0][0]==0){
+            // 第一行 第一列都得变成0 ，但是通过变量延迟变化
+            rowSet = true;
+            colSet = true;
+        }else{
+            for(int row = 0;row<matrix.length;row++){
+                if(matrix[row][0]==0){
+                    rowSet = true;
+                    break;
+                }
+
+            }
+            for(int col = 0;col<matrix[0].length;col++){
+                if(matrix[0][col]==0){
+                    colSet = true;
+                    break;
+                }
+
+            }
+
+        }
+
+        // 如果其他行或者列需要变成0，用 第一行或者第一列来做记号
+        for(int i = 1;i<matrix.length;i++){
+            for(int j = 1;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // 通过头部是否是0，来将行和列 变为 0
+        // for(int row = 0;row<matrix.length;row++){
+        //     for(int col = 0;col<matrix[0].length;col++){
+        //         if(matrix[row][0]==0||matrix[0][col]==0){
+        //             matrix[row][col] = 0;
+        //         }
+        //     }
+        // }
+        for (int row = matrix.length-1; row >= 0; row--) {
+            for (int col = matrix[0].length-1; col >=0 ; col--) {
+                if(matrix[row][0] == 0 || matrix[0][col] == 0){
+                    matrix[row][col] = 0;
+                }
+            }
+        }
+
+        // 第一行 第一列 判断
+
+        if(rowSet){
+            for(int i = 0;i<matrix.length;i++){
+                matrix[i][0] = 0;
+            }
+        }
+        if(colSet){
+            for(int i = 0;i<matrix[0].length;i++){
+                matrix[0][i] = 0;
+            }
+        }
+
+
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+        setZerosSimple(matrix);
     }
 }
--- SlidingWindow/LongestOnes.java
+++ SlidingWindow/LongestOnes.java
@@ -1,7 +1,7 @@
 /**
  *  1004
  */
-public class longestOnes {
+public class LongestOnes {
     /**
      *  滑动窗口解决问题
      *  时复：O（n）
@@ -48,17 +48,64 @@ public class longestOnes {
         int left = 0;
         int right = 0;
         while(right<A.length){
-            
             if(A[right++]==0) k--;
             if(k<0&&A[left++]==0) k++;
         }
         return right-left;
     }
 
+    /**
+     * 二分法解决问题
+     * @param A
+     * @param K
+     * @return
+     */
+    public static int longestOneWithBinary(int[] A,int K){
+        int n  = A.length;
+        int res = 0;
+        // 记录到 i 点，有几个0；
+        int[] p = new int[n+1];
+        for(int i = 1;i<=n;++i){
+            p[i] = p[i-1]+(1-A[i-1]);
+        }
+       for(int right = 0;right<n;++right){
+           
+           int left = binarySearch(p, p[right+1]-K);
+           res = Math.max(right-left+1,res);
+
+       }
+        return res;
+
+
+
+    }
+    /**
+     *  p 单调递增是因为 0只会越来越多
+     *  二分实现
+     */
+    public static int binarySearch(int[] p,int target){
+        int low = 0;
+        int high = p.length;
+        while(low<high){
+            int mid = (high-low)/2+low;
+            if(p[mid]<target){
+                low = mid+1;
+            }else{
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+
+    
+
+
+
 
     public static void main(String[] args) {
         int[] A = {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
         int K = 3;
-        System.out.println(longestOnes(A, K));
+        System.out.println(longestOneWithBinary(A, K));
     }    
 }
